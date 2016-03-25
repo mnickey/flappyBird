c0f299e433c73da23730ed943bc6857e31b70344
Michael Nickey <mnickey@gmail.com>
2016-03-25 14:16:28 -0700

    first commit

---
+++ flappy.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+surface = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Helicopter')
+
+clock = pygame.time.Clock()
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
+quit()
